4169a444942f5bd10c01de8dcb2d64059c4a7ea9
Anant Dubey <anantkumar312@gmail.com>
2024-01-17 14:00:10 +0530

    feat: made cards dynamic

--- lib/widgets/background_card.dart
+++ lib/widgets/background_card.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 
 class BackgroundCard extends StatelessWidget {
-  const BackgroundCard({super.key});
+  const BackgroundCard({
+    super.key,
+    required this.destination,
+    required this.idealDuration,
+    required this.imageURL,
+    required this.placesToVisit,
+    required this.tagline,
+    required this.timeToVisit,
+  });
+
+  final String imageURL;
+  final String destination;
+  final String tagline;
+  final String timeToVisit;
+  final String idealDuration;
+  final String placesToVisit;
 
   @override
   Widget build(BuildContext context) {
@@ -13,32 +28,40 @@ class BackgroundCard extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
         child: Row(
           children: [
-            Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: Image.network(
-                  "https://images.unsplash.com/photo-1598091383021-15ddea10925d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                  width: 150,
-                  height: 150,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              const SizedBox(height: 16),
-              Text(
-                'Kashmir',
-                style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                      fontWeight: FontWeight.bold,
-                      color: Theme.of(context).colorScheme.onSecondaryContainer,
+            Expanded(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: Image.network(
+                        imageURL,
+                        width: 150,
+                        height: 150,
+                        fit: BoxFit.cover,
+                      ),
                     ),
-              ),
-              Text(
-                'The ice on the cake',
-                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                      fontStyle: FontStyle.italic,
-                      color: Theme.of(context).colorScheme.onSecondaryContainer,
+                    const SizedBox(height: 16),
+                    Text(
+                      destination,
+                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context)
+                                .colorScheme
+                                .onSecondaryContainer,
+                          ),
                     ),
-              ),
-            ]),
+                    Text(
+                      tagline,
+                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            fontStyle: FontStyle.italic,
+                            color: Theme.of(context)
+                                .colorScheme
+                                .onSecondaryContainer,
+                          ),
+                    ),
+                  ]),
+            ),
             const SizedBox(width: 16),
             Expanded(
               child: Column(
@@ -54,7 +77,7 @@ class BackgroundCard extends StatelessWidget {
                           ),
                     ),
                     Text(
-                      'March to October & from December to February',
+                      timeToVisit,
                       style: Theme.of(context).textTheme.bodySmall!.copyWith(
                             color: Theme.of(context)
                                 .colorScheme
@@ -72,7 +95,7 @@ class BackgroundCard extends StatelessWidget {
                           ),
                     ),
                     Text(
-                      '4 to 5 days',
+                      idealDuration,
                       style: Theme.of(context).textTheme.bodySmall!.copyWith(
                             color: Theme.of(context)
                                 .colorScheme
@@ -90,7 +113,7 @@ class BackgroundCard extends StatelessWidget {
                           ),
                     ),
                     Text(
-                      'Srinagar, Gulmarg, Jammu City, Sonamarg, Leh, Pahalgam, Pulwama',
+                      placesToVisit,
                       style: Theme.of(context).textTheme.bodySmall!.copyWith(
                             color: Theme.of(context)
                                 .colorScheme
