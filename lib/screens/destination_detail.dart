b06851ac27613857093dbd13cc0844b3c1e55f71
Anant Dubey <anantkumar312@gmail.com>
2024-01-18 13:13:04 +0530

    feat: change detail screen to show dummy quote

--- lib/screens/destination_detail.dart
+++ lib/screens/destination_detail.dart
@@ -14,7 +14,27 @@ class DestinationDetail extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(destinationTitle)),
-      body: Center(child: Image.network(imageURL)),
+      body: Center(
+          child: Column(
+        children: [
+          Card(
+            margin: const EdgeInsets.all(24),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Image.network(
+                imageURL,
+                height: 250,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          const SizedBox(height: 16),
+          const Text('A random quote'),
+        ],
+      )),
     );
   }
 }
