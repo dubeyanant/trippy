78abc97d59686556ea6f92bf160c4bc5d9ac4714
Anant Dubey <anantkumar312@gmail.com>
2024-01-18 14:59:52 +0530

    fix: handle error on quote loading
    before error showed on screen because json quote had escape character, now a default quote is shown on screen

--- lib/widgets/random_quote.dart
+++ lib/widgets/random_quote.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'dart:convert';
+import 'dart:developer' as developer;
 
 import 'package:http/http.dart' as http;
 
@@ -8,6 +9,7 @@ class RandomQuote extends StatelessWidget {
   const RandomQuote({super.key});
 
   Future<Map<String, dynamic>> fetchQuote() async {
+    // Open API: https://forismatic.com/en/api/
     final response = await http.get(Uri.parse(
         'http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en'));
 
@@ -26,14 +28,35 @@ class RandomQuote extends StatelessWidget {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const CircularProgressIndicator();
         } else if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}');
+          developer.log(
+            'Error:',
+            name: 'Quote loading failed',
+            error: snapshot.error,
+          );
+          return Column(
+            children: [
+              Text(
+                "I think, therefore I am.",
+                style: Theme.of(context).textTheme.bodyMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 16),
+              Text(
+                "~ René Descartes's",
+                style: Theme.of(context)
+                    .textTheme
+                    .bodySmall!
+                    .copyWith(fontStyle: FontStyle.italic),
+              )
+            ],
+          );
         } else {
           Map<String, dynamic> quoteData =
               snapshot.data as Map<String, dynamic>;
 
-          String quoteText =
-              quoteData['quoteText'] ?? 'No quote text available';
-          String quoteAuthor = quoteData['quoteAuthor'] ?? 'Anonymous';
+          String quoteText = quoteData['quoteText'] ?? 'No quote available';
+          String quoteAuthor =
+              quoteData['quoteAuthor'] ?? 'No author available';
 
           return Column(
             children: [
